12e3556d60e56d76373ce9af51d156194794c144
chrisGaye <gayetalib90@gmail.com>
2020-07-05 11:04:10 +0000

    useSate

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState } from "react";
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+ const [name, setname] = useState('Gaye'); 
+
+ const handleClick = () => {
+    setname('Amadou')
+ }
+ 
   return (
     <View style={styles.container}>
         <View style={styles.header}>
-             <Text style={styles.name}>Amadou GAYE</Text>
+            <Text style={styles.name2}>Yes my name is {name} </Text>
+
+            <View style={styles.buttonContainer}> 
+               <Button title="Update state" onPress={handleClick}/>
+            </View>
         </View>
       <StatusBar style="auto" />
     </View>
@@ -24,8 +35,11 @@ const styles = StyleSheet.create({
     backgroundColor : 'yellow',
     padding: 20,
   },
-  name: {
+  name2: {
     color: 'black',
     fontWeight: 'bold'
+  },
+  buttonContainer: {
+    marginTop: 20,
   }
 });
